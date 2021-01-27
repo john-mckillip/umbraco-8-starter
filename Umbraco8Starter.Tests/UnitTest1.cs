88340870fb4c9e8806bc5d06c99451777a352a53
John McKillip <john0mckillip@gmail.com>
2021-01-27 13:58:33 -0600

    Add project files.

---
+++ Umbraco8Starter.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Umbraco8Starter.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
